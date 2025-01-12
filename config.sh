fce85655799fd6aa38d09d9ba3cd3a80b839e68d
micropuma <micropuma@163.com>
2025-01-13 00:12:56 +0800

    revise config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 export PATH=$PATH:/home/douliyang/large/mlir-workspace/torch-mlir/build/bin
+export PYTHONPATH=`pwd`/build/tools/torch-mlir/python_packages/torch_mlir:`pwd`/test/python/fx_importer
